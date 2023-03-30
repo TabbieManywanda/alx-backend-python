40e3fda1c13a66cab49a8237f71306c378a04a65
TabbieManywanda <achiengtabitha@gmail.com>
2023-03-30 10:38:12 +0000

    Returns string from float argument

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''Annotated function to_str that returns a string'''
+
+
+def to_str(n: float) -> str:
+    '''A function that takes in a float as argument
+    and returns a string representation of the float'''
+    return str(n)
