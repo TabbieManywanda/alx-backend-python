0aa5b42142977c60b5e4f7eec009d18c861f3eb5
TabbieManywanda <achiengtabitha@gmail.com>
2023-03-30 10:24:21 +0000

    Function that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''Writing an annotated concat function'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Function that takes in two strings as arguments
+    and returns a concatenated string of the two'''
+    return str1 + str2
