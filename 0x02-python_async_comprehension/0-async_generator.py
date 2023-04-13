30608fbb80ce18ee14b52277d2e2df9bbd26face
TabbieManywanda <achiengtabitha@gmail.com>
2023-04-13 09:37:21 +0000

    Writing a coroutine

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''Writing a coroutine'''
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''The coroutine will loop 10 times, each time asynchronously
+    wait 1 second, then yield a random number between 0 and 10'''
+
+    for x in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
